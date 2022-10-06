2765122b284284a98421bd5afbf17fa0926ec9db
Ivan Garcia <igarcia@strands.com>
2022-10-06 18:26:44 +0200

    Add Shopping list create/edit/remove functionalities

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) ingredientNameRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) ingredientAmountRef: ElementRef;
+  @ViewChild('unitInput', {static: false}) ingredientUnitRef: ElementRef;
+
+  @Output() ingredientAdded   = new EventEmitter<Ingredient>;
+  @Output() ingredientEdited  = new EventEmitter<Ingredient>;
+  @Output() ingredientRemoved = new EventEmitter<Ingredient>;
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onIngredientCreate() {
+    const createdIngredient = new Ingredient(
+      this.ingredientNameRef.nativeElement.value,
+      this.ingredientAmountRef.nativeElement.value,
+      this.ingredientUnitRef.nativeElement.value
+    )
+
+    this.ingredientAdded.emit(createdIngredient);
+  }
+
+  onIngredientEdit() {
+    const createdIngredient = new Ingredient(
+      this.ingredientNameRef.nativeElement.value,
+      this.ingredientAmountRef.nativeElement.value,
+      this.ingredientUnitRef.nativeElement.value
+    )
+
+    this.ingredientEdited.emit(createdIngredient);
+  
+  }
+
+  onIngredientDelete() {
+    const createdIngredient = new Ingredient(
+      this.ingredientNameRef.nativeElement.value,
+      this.ingredientAmountRef.nativeElement.value,
+      this.ingredientUnitRef.nativeElement.value
+    )
+
+    this.ingredientRemoved.emit(createdIngredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -23,4 +23,44 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(newIngredient: Ingredient) {
+    if(this.findInIngredientList(newIngredient)) {
+      console.error("Already present");
+    } else {
+      this.ingredientList.push(newIngredient)
+    }
+  }
+
+  onIngredientEdited(newIngredient: Ingredient) {
+    if(this.findInIngredientList(newIngredient) == null) {
+      console.error("Element to edit not present in list");
+    } else {    
+      this.findInIngredientList(newIngredient).amount = newIngredient.amount;
+      this.findInIngredientList(newIngredient).unit = newIngredient.unit;
+    }
+  }
+
+  onIngredientRemoved(newIngredient: Ingredient) {
+    let toDelete: Ingredient = this.findInIngredientList(newIngredient)
+
+    if(toDelete == null) {
+      console.error("Element to delete not present in list");
+      return;
+    }
+    
+    this.ingredientList.splice(this.ingredientList.indexOf(toDelete), 1)
+  }
+
+
+  /**
+   * Finds ingredient in list if present
+   * 
+   * @param newIngredient 
+   * @returns Ingredient 
+   */
+  findInIngredientList(newIngredient: Ingredient) {
+    return this.ingredientList.find((obj) => {
+      return obj.name === newIngredient.name;
+    });
+  }
 }
