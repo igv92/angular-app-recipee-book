8cb964e0c525387291435ecd33cc87cdcdea28e2
igv92 <garciavascoivan@gmail.com>
2023-06-16 13:10:17 +0200

    Test push to remote

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -13,7 +13,7 @@ export class ShoppingListComponent implements OnInit {
 
   constructor() {
     this.ingredientList = [
-      new Ingredient('Meat', 200, 'grams'),
+      new Ingredient('Meat', 100, 'grams'),
       new Ingredient('Mayo', 20, 'grams'),
       new Ingredient('Bread', 1, 'burger bread'),
       new Ingredient('Lettuce', null, null)
@@ -34,7 +34,7 @@ export class ShoppingListComponent implements OnInit {
   onIngredientEdited(newIngredient: Ingredient) {
     if(this.findInIngredientList(newIngredient) == null) {
       console.error("Element to edit not present in list");
-    } else {    
+    } else {
       this.findInIngredientList(newIngredient).amount = newIngredient.amount;
       this.findInIngredientList(newIngredient).unit = newIngredient.unit;
     }
@@ -47,16 +47,16 @@ export class ShoppingListComponent implements OnInit {
       console.error("Element to delete not present in list");
       return;
     }
-    
+
     this.ingredientList.splice(this.ingredientList.indexOf(toDelete), 1)
   }
 
 
   /**
    * Finds ingredient in list if present
-   * 
-   * @param newIngredient 
-   * @returns Ingredient 
+   *
+   * @param newIngredient
+   * @returns Ingredient
    */
   findInIngredientList(newIngredient: Ingredient) {
     return this.ingredientList.find((obj) => {
